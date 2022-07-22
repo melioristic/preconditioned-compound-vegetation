caee7b77fc389c4d25a74bacd95be052c00b23f2
melioristic <itsmohitanand@gmail.com>
2022-07-22 09:08:06 +0200

    renamed notebook

--- s1_regrid_landuse.py
+++ s1_regrid_landuse.py
@@ -4,5 +4,4 @@ landuse_path = "/Users/anand/Documents/data/project_3_data/ESACCI-LC-L4-L_fromLU
 temperature_path = "/Users/anand/Documents/data/project_3_data/t2m.monthly.era5.europe.1981-2020.nc"
 
 regridded_landuse = regrid_data(landuse_path, temperature_path)
-
 regridded_landuse.to_netcdf("/Users/anand/Documents/data/project_3_data/regridded_LU_2018.nc")
