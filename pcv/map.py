3df5cc893fd739cbf64987dece4b6aa1a771e307
anand <mohit.anand@ufz.de>
2022-10-13 09:45:03 +0200

    added correlation plot

--- pcv/map.py
+++ pcv/map.py
@@ -183,7 +183,7 @@ class SEMMap(object):
             valE = self._get_piping_val(piping[0])
             for each_piping in piping[1:]:
                 valE += self._get_piping_val(each_piping)
-            max_min = 0.5
+            max_min = 0.2
         else:
             valE = self._get_piping_val(piping)
 
