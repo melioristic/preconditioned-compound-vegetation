b56f63ecd1b4d243af6058cf11580e67e010f492
anand <mohit.anand@ufz.de>
2023-07-31 21:28:38 +0200

    added log coef

--- s8_weather_xtreme.py
+++ s8_weather_xtreme.py
@@ -7,7 +7,7 @@ from pcv.io import read_ipcc_region_csv
 from pathlib import Path
 from sklearn.metrics import roc_auc_score
 
-vegetation_type = "crop"
+vegetation_type = "forest"
 xtreme = "high"
 
 root_folder = Path("/data/compoundx/anand/PCV/data/")
@@ -67,6 +67,9 @@ for region_fdir in csv_folder.iterdir():
 
         auc_winter = []
         auc_without_winter = []
+
+        winter_coefficient = np.zeros((100, 8))
+        coefficient = np.zeros((100,6))
         for n in range(100):
             train_data, val_data, test_data = read_ipcc_region_csv(region_fdir)        
             train_data = train_data[:,keep_col_index, 0]
@@ -74,29 +77,43 @@ for region_fdir in csv_folder.iterdir():
             test_data = test_data[:,keep_col_index, 0]
             clf, X, Y = clf_estimator(train_data, val_data, winter=True)
             Y_score = clf.predict_proba(X)[:,1]
-            auc_winter.append(roc_auc_score(Y, Y_score))
+            auc = roc_auc_score(Y, Y_score)
+            auc_winter.append(auc)
+            winter_coefficient[n, 0] = clf.intercept_
+            winter_coefficient[n, 1:7] = clf.coef_
+            winter_coefficient[n, 7] = auc
 
             clf, X, Y = clf_estimator(train_data, val_data, winter=False)
             Y_score = clf.predict_proba(X)[:,1]
-            auc_without_winter.append(roc_auc_score(Y, Y_score))
+            auc = roc_auc_score(Y, Y_score)
+            auc_without_winter.append(auc)
             
+            coefficient[n, 0] = clf.intercept_
+            coefficient[n, 1:5] = clf.coef_
+            coefficient[n, 5] = auc
+
             RocCurveDisplay.from_estimator(*clf_estimator(train_data, val_data, winter=True) , ax=axes[1],**{"color":"r", "alpha":0.2} )
             RocCurveDisplay.from_estimator(*clf_estimator(train_data, val_data, winter=False) , ax=axes[1],**{"color":"b", "alpha":0.2} )
             
             axes[1].get_legend().remove()
 
+        region = str(region_fdir).split("/")[-1][:-4]
 
+        header_1 = "a0\tt2m_winter\ttp_winter\tt2m_spring\ttp_spring\tt2m_summer\ttp_summer\tAUC"
+        header_2 = "a0\tt2m_spring\ttp_spring\tt2m_summer\ttp_summer\tAUC"
+
+        np.savetxt(f"/data/compoundx/anand/PCV/data/{vegetation_type}_data/{xtreme}/logreg_winter_{xtreme}_{region}.csv", winter_coefficient, fmt='%1.3f', delimiter="\t", header=header_1)
+        np.savetxt(f"/data/compoundx/anand/PCV/data/{vegetation_type}_data/{xtreme}/logreg_{xtreme}_{region}.csv", coefficient, fmt='%1.3f', delimiter="\t", header=header_2)
+        
         axes[2].hist(auc_winter, color = "red", bins=20, alpha=0.5)
         axes[2].hist(auc_without_winter, color="blue", bins=20, alpha=0.5)
         axes[2].axvline(np.percentile(auc_without_winter, 90), color = "k", linestyle = "--", label = "90th percentile AUC | Without Winter ")
         axes[2].axvline(np.mean(auc_winter), color="k", label = "mean AUC | With Winter ")
         axes[2].legend()
 
-        region = str(region_fdir).split("/")[-1][:-4]
 
         fig.suptitle(f"{xtreme}_{region} | With Winter = Red | Without Winter = Blue")
         plt.tight_layout()
-
         print(f"Saved Figure for the region {region}")
         plt.savefig(f"/data/compoundx/anand/PCV/images/{vegetation_type}/{xtreme}/only_weather_{xtreme}_{region}.png")
         plt.close()
